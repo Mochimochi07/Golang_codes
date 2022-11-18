2b6a5754337b0391a7f40e9c5ac76c260650c196
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2022-11-18 16:29:18 +0800

    Create scan.go

---
+++ programs/scan.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main() {
+
+var s1 string
+		var s2 string
+		var s3 string
+		
+		fmt.Scan(&s1, &s2, &s3)
+		fmt.Println("s1:", s1, "s2:", s2, "s3:", s3)
+  
+  
+}
